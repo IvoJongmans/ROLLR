0f023b7899211ac1887e8c2a5361da67f3db1938
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-17 19:41:04 +0200

    added pgsql to heroky. along with seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        App\Scooter::insert([
+            'location' => 'Assen',
+        ]);
     }
 }
