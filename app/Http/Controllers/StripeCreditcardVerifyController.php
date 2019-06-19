364f5d3a5c127aaef312e1aca007348180c73ac1
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-19 11:42:01 +0200

    starting on CC validation

--- app/Http/Controllers/StripeCreditcardVerifyController.php
+++ app/Http/Controllers/StripeCreditcardVerifyController.php
@@ -8,7 +8,9 @@ class StripeCreditcardVerifyController extends Controller
 {
     public function handle(Request $request) {
         
-        return $request;
+        $stripe_id = $request->data['object']['customer'];
+        User::where('stripe_id', $stripe_id)->update(array('cc_validated' => 'yes'));
+        return "Creditcard was validated";
 
     }
 }
