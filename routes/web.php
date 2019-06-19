f81bf9b1214f46bbe839e71ee3b071ba0cecdb94
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-19 18:30:30 +0200

    trying to add auth

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('scooter/{scooter}/register', 'UserRegisterController@index');
 Route::post('scooter/{scooter}/register', 'UserRegisterController@store');
 
-Route::get('scooter/{scooter}/login', 'UserLoginController@index');
+Route::get('scooter/{scooter}/login', 'UserLoginController@index')->middleware('auth');
 Route::post('scooter/{scooter}/login', 'UserLoginController@login');
 
 Route::get('scooter/{scooter}/verify/{user}', 'VerifyUserController@verify');
