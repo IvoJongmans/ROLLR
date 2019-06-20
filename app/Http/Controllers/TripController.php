fd2d49e49bfc33653f44b08bfce7f688152c79b2
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-20 20:17:29 +0200

    added trips DB

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -98,10 +98,10 @@ class TripController extends Controller
         Trip::where('id', $trip->id)->update(['updated_at' => \Carbon\Carbon::now()]);        
         $stripe_id = User::where('id', $user->id)->value('stripe_id');
 
-        $trip_time = Trip::where("id", $trip->id)->select(DB::raw("TIMESTAMPDIFF(SECOND,created_at, updated_at) as diff_in_secs"))->get()->pluck("diff_in_secs");
+        // $trip_time = Trip::where("id", $trip->id)->select(DB::raw("TIMESTAMPDIFF(SECOND,created_at, updated_at) as diff_in_secs"))->get()->pluck("diff_in_secs");
         
-        $seconds = ($trip_time[0]);
-        $minutes = floor($seconds / 60);
+        // $seconds = ($trip_time[0]);
+        // $minutes = floor($seconds / 60);
         $amount = 100 + ($minutes * 15);
         
         
