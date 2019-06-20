cc0148e18b0caa65c27c5f6474274ef3fb44aa25
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-20 20:13:30 +0200

    added trips DB

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Trip;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use App\Scooter;
 use App\User;
 use Stripe;
