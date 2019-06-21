581f022ae717adf7887596a66e64adac0299790d
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-21 13:19:14 +0200

    with map

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -119,7 +119,8 @@ input:checked + .slider:before {
   border-radius: 50%;
 }
 #map {
-  height: 100%;
+  height: 200px;
+  width: 200px;
 }
 @endsection
 
@@ -167,56 +168,7 @@ input:checked + .slider:before {
             </div>
           </form>
     </div>
-    <script>
-        // Note: This example requires that you consent to location sharing when
-        // prompted by your browser. If you see the error "The Geolocation service
-        // failed.", it means you probably did not give permission for the browser to
-        // locate you.
-        var map, infoWindow;
-  
-        function initMap() {
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 53.214902, lng: 6.564738},
-            zoom: 17
-          });
-          infoWindow = new google.maps.InfoWindow;
-          // map.setCenter(pos);
-        };
-  
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-          infoWindow.open(map);
-        };
-      </script>
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7ACuBSISEBf6cm57NPd7FQalB66VV3-s&callback=initMap"
-      async defer></script>
-      <script>
-      setInterval(updateMap, 2000);
-      function updateMap() {
-          URL = "/map/retrieve";
-          submitdata = {id: 1};
-          $.post(URL, submitdata, function(data){
-              latitude = data['lat'],
-              longitude = data['lng']
-              console.log(latitude);
-              console.log(longitude); 
-              var pos = {
-                  lat: parseFloat(latitude),
-                  lng: parseFloat(longitude)
-              }
-          console.log(pos);
-          map.setCenter(pos);
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Scooter location');
-          infoWindow.open(map); 
-          }); 
-          // number++; 
-          // console.log(number);
-        }
-      </script>
+    
     
 
     <script>
@@ -388,7 +340,56 @@ input:checked + .slider:before {
         }
       });
     </script>
-
+    <script>
+        // Note: This example requires that you consent to location sharing when
+        // prompted by your browser. If you see the error "The Geolocation service
+        // failed.", it means you probably did not give permission for the browser to
+        // locate you.
+        var map, infoWindow;
+  
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 53.214902, lng: 6.564738},
+            zoom: 17
+          });
+          infoWindow = new google.maps.InfoWindow;
+          // map.setCenter(pos);
+        };
+  
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+          infoWindow.open(map);
+        };
+      </script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7ACuBSISEBf6cm57NPd7FQalB66VV3-s&callback=initMap"
+      async defer></script>
+      <script>
+      setInterval(updateMap, 2000);
+      function updateMap() {
+          URL = "/map/retrieve";
+          submitdata = {id: 1};
+          $.post(URL, submitdata, function(data){
+              latitude = data['lat'],
+              longitude = data['lng']
+              console.log(latitude);
+              console.log(longitude); 
+              var pos = {
+                  lat: parseFloat(latitude),
+                  lng: parseFloat(longitude)
+              }
+          console.log(pos);
+          map.setCenter(pos);
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Scooter location');
+          infoWindow.open(map); 
+          }); 
+          // number++; 
+          // console.log(number);
+        }
+      </script>
     <img src="/images/scooter.png" class="fix">
     
 @endsection
