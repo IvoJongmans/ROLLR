dfc72681dbf7d1b26d6652cfd3fcf3d37113145e
Jeroen de Reus <48788831+jdereus87@users.noreply.github.com>
2019-06-21 13:03:21 +0200

    push map script

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -118,7 +118,9 @@ input:checked + .slider:before {
 .slider.round:before {
   border-radius: 50%;
 }
-    
+#map {
+  height: 100%;
+}
 @endsection
 
 @section('content')
@@ -139,6 +141,7 @@ input:checked + .slider:before {
     </div>
   </div>
 </div>
+
 
     <div class="container text-right">
       {{-- <button href="{{ route('logout', compact('scooter')) }}"><button class="custom-button">Logout</button></button> --}}
@@ -164,7 +167,57 @@ input:checked + .slider:before {
             </div>
           </form>
     </div>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script>
+        // Note: This example requires that you consent to location sharing when
+        // prompted by your browser. If you see the error "The Geolocation service
+        // failed.", it means you probably did not give permission for the browser to
+        // locate you.
+        var map, infoWindow;
+  
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 53.214902, lng: 6.564738},
+            zoom: 17
+          });
+          infoWindow = new google.maps.InfoWindow;
+          // map.setCenter(pos);
+        };
+  
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+          infoWindow.open(map);
+        };
+      </script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7ACuBSISEBf6cm57NPd7FQalB66VV3-s&callback=initMap"
+      async defer></script>
+      <script>
+      setInterval(updateMap, 2000);
+      function updateMap() {
+          URL = "/map/retrieve";
+          submitdata = {id: 1};
+          $.post(URL, submitdata, function(data){
+              latitude = data['lat'],
+              longitude = data['lng']
+              console.log(latitude);
+              console.log(longitude); 
+              var pos = {
+                  lat: parseFloat(latitude),
+                  lng: parseFloat(longitude)
+              }
+          console.log(pos);
+          map.setCenter(pos);
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Scooter location');
+          infoWindow.open(map); 
+          }); 
+          // number++; 
+          // console.log(number);
+        }
+      </script>
     
 
     <script>
@@ -255,10 +308,13 @@ input:checked + .slider:before {
     </div>
 
     @endif
-
+    <div id="map"></div>
     <div class="container" id="trip_json">
     
     </div>
+
+    
+    
 
     <script>
       var seconds = 0;
