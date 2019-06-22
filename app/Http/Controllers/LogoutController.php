eb3531297b87938a66ffe2dcbd183b26ab4c6085
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-22 19:56:35 +0200

    updated laravel

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    public function logout () {
+    public function logout() {
         
         auth()->logout();
         
