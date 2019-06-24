15de5cdabd43f77be04c97bfac6590f1c020c092
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-24 15:22:49 +0200

    update with route to homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+
+@section('style')
+
+    body {
+        color:white;
+        background-color: #001B2B;
+    }
+
+@endsection
+
+@section('content')
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        
+        RL here
+      </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">How it works</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Our scooters
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Falcon</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Xiaomi</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+      </ul>>
+    </div>
+  </nav>
+
+@endsection
