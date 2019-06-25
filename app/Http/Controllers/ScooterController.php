e54e4bc806e77f96526ad942897b42ea6f01bae5
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-25 12:28:48 +0200

    closed the loop

--- app/Http/Controllers/ScooterController.php
+++ app/Http/Controllers/ScooterController.php
@@ -100,19 +100,21 @@ class ScooterController extends Controller
         if(
             (time() - strtotime($timecheck[0]['updated_at'])) > 310
             ){
-        $json = json_decode(file_get_contents('https://portaallovetracking.com/api_po/1.php?api=pl&ver=1.5&key=' . env('API_KEY_TRACKER') . '&cmd=OBJECT_GET_POSITION,*'), true); 
-        foreach($json as $key => $tracker){
-            $scooterid = Scooter::where('imei', '=', $key)->first()->id;  
-            $scooterupdate = Scooter::find($scooterid);
-            $scooterupdate->latitude = $tracker['lat'];
-            $scooterupdate->longitude = $tracker['lng'];
-            $scooterupdate->save(); 
-        };
-        $scooter = Scooter::all();
-        return response()->json($scooter); 
+
+                    $json = json_decode(file_get_contents('https://portaallovetracking.com/api_po/1.php?api=pl&ver=1.5&key=' . env('API_KEY_TRACKER') . '&cmd=OBJECT_GET_POSITION,*'), true); 
+                    foreach($json as $key => $tracker){
+                        $scooterid = Scooter::where('imei', '=', $key)->first()->id;  
+                        $scooterupdate = Scooter::find($scooterid);
+                        $scooterupdate->latitude = $tracker['lat'];
+                        $scooterupdate->longitude = $tracker['lng'];
+                        $scooterupdate->save(); 
+                    };
+                    $scooter = Scooter::all();
+                    return response()->json($scooter);
+
         } else {   
-          $scooter = Scooter:all();
+          $scooter = Scooter::all();
           return response()->json($scooter);
-          
+        }
     }
 }
