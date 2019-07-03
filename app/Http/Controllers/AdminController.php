7dca49f4d39f60e53c61e500bf5d5480195e0407
Jeroen de Reus <48788831+jdereus87@users.noreply.github.com>
2019-07-03 12:03:15 +0200

    user model updated and new update migration for users table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,28 +10,32 @@ use App\Admin;
 
 class AdminController extends Controller
 {
+// Admin Panel with User model and default login
 
-    public function home(){
-        return view('admin/home');
-    }
-    public function login(Request $request)
-    {
-        $username = $request['username'];
-        $password = $request['password']; 
 
-        $database_password = Admin::where('username', $request['username'])->value('password');
-        $user_id = Admin::where('username', $request['username'])->value('id');
-        $user = Admin::where('username', $request['username']);
 
-        if ($database_password == $request['password']){
-        Auth::guard('admin')->loginUsingId($user_id);
-        return redirect()->route('test');
-        }
-        else return  redirect()->route('admin'); 
-    }
-    public function test()
-    {   
-        return view('admin/test'); 
-    }
+// Admin Panel with custom login and custom model
+    // public function home(){
+    //     return view('admin/home');
+    // }
+    // public function login(Request $request)
+    // {
+    //     $username = $request['username'];
+    //     $password = $request['password']; 
+
+    //     $database_password = Admin::where('username', $request['username'])->value('password');
+    //     $user_id = Admin::where('username', $request['username'])->value('id');
+    //     $user = Admin::where('username', $request['username']);
+
+    //     if ($database_password == $request['password']){
+    //     Auth::guard('admin')->loginUsingId($user_id);
+    //     return redirect()->route('test');
+    //     }
+    //     else return  redirect()->route('admin'); 
+    // }
+    // public function test()
+    // {   
+    //     return view('admin/test'); 
+    // }
 }
 
